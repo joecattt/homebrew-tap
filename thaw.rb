22a79efe41700fad5e3fd0853fd94d6634eeeb49
Joseph Reyna <joecat@MacBookPro.home.local>
2026-04-02 18:45:44 -0500

    fix: add sha256 checksums for v3.3.1

--- thaw.rb
+++ thaw.rb
@@ -7,16 +7,20 @@ class Thaw < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/joecattt/thaw/releases/download/v3.3.1/thaw_3.3.1_darwin_arm64.tar.gz"
+      sha256 "a18ac1fe14acb7c2188fc0e2a5dbce85f5a947b2a90b65d493d2011ee7cd1a28"
     else
       url "https://github.com/joecattt/thaw/releases/download/v3.3.1/thaw_3.3.1_darwin_amd64.tar.gz"
+      sha256 "a31d94c44dd0c3e0eee8929282e09d31a133e8a4ae10a7a2d8e60347137c7175"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/joecattt/thaw/releases/download/v3.3.1/thaw_3.3.1_linux_arm64.tar.gz"
+      sha256 "2c7ad0ad089a4cc9a0681750da05e069b2702a9bdaa4a0153cb30687e24decd9"
     else
       url "https://github.com/joecattt/thaw/releases/download/v3.3.1/thaw_3.3.1_linux_amd64.tar.gz"
+      sha256 "86859e261577656460a4fe32badc911ca25d0d3c14588508f0493c6a988c1b84"
     end
   end
 
